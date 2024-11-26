e2848395196b44667f0f5134df1c98864b81ea0a
RandomPancakeBoi <76412682+RandomPancakeBoi@users.noreply.github.com>
2024-11-26 13:35:54 +0000

    Add files via upload

--- contact.js
+++ contact.js
@@ -13,36 +13,4 @@ document.addEventListener("DOMContentLoaded", () => {
 
     // Initial check when the form is loaded
     checkFormValidity();
-
-    form.addEventListener("submit", (e) => {
-        e.preventDefault();
-
-        // Collect form data
-        const formData = new FormData(form);
-        
-        // Log form data to check what is being submitted
-        for (let [key, value] of formData.entries()) {
-            console.log(key + ": " + value);
-        }
-
-        // Send the form data
-        fetch("https://formsubmit.co/goodwin3117+forms@gmail.com", {
-            method: "POST",
-            body: formData,
-        })
-        .then(response => {
-            console.log('Response Status: ', response.status);
-            if (response.ok) {
-                alert("Form submitted successfully!");
-                form.reset();  // Reset form fields
-                submitButton.disabled = true;  // Disable the submit button after successful submit
-            } else {
-                alert("There was an error submitting the form.");
-            }
-        })
-        .catch((error) => {
-            console.error('Form submission error:', error);
-            alert("There was an error submitting the form.");
-        });
-    });
 });
