bae5f3132c67c29c9f8584b7710a9ae7d4f0e681
RandomPancakeBoi <76412682+RandomPancakeBoi@users.noreply.github.com>
2024-12-08 19:30:10 +0000

    Add files via upload

---
+++ reviews.js
@@ -0,0 +1,14 @@
+// Initalise
+const swuper = new Swiper('.swiper', {
+    loop: true,
+
+    // For Pages
+    pagination: {
+        el: '.swiper-pagination'
+    },
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+})
