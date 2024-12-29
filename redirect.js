23b1945147d90db4d3fd9b5c70237deab5f36b17
RandomPancakeBoi <76412682+RandomPancakeBoi@users.noreply.github.com>
2024-12-29 11:55:05 +0000

    Add files via upload

--- redirect.js
+++ redirect.js
@@ -1,17 +1,17 @@
 // Redirect users to the appropriate version of the site based on device type.
 (function() {
-    const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
-    const currentPath = window.location.pathname;
+  const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+  const currentPath = window.location.pathname;
 
-    // Define paths for mobile and desktop
-    const mobilePath = '/mobile-index.html';
-    const desktopPath = '/index.html';
+  // Define paths for mobile and desktop
+  const mobilePath = '/mobile-index.html';
+  const desktopPath = '/index.html';
 
-    if (isMobile && !currentPath.includes('mobile-index.html')) {
-        // Redirect to mobile version if the user is on a mobile device
-        window.location.replace(mobilePath);
-    } else if (!isMobile && !currentPath.includes('index.html')) {
-        // Redirect to desktop version if the user is on a desktop
-        window.location.replace(desktopPath);
-    }
+  if (isMobile && !currentPath.includes('mobile-index.html')) {
+      // Redirect to mobile version if the user is on a mobile device
+      window.location.replace(mobilePath);
+  } else if (!isMobile && !currentPath.includes('index.html')) {
+      // Redirect to desktop version if the user is on a desktop
+      window.location.replace(desktopPath);
+  }
 })();
